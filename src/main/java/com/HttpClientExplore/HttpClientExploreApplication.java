3f7aff67b956f1289f4a5076140e69f75bfd1704
kbtutorials <gauthamkarthik534@gmail.com>
2023-09-14 08:08:25 +0530

    HttpClient

---
+++ src/main/java/com/HttpClientExplore/HttpClientExploreApplication.java
@@ -0,0 +1,14 @@
+package com.HttpClientExplore;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HttpClientExploreApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpClientExploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HttpClientExplore/HttpClientExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HttpClientExplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
