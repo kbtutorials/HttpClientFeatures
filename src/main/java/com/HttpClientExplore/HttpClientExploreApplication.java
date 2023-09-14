a771974fe33b3c22663be358ba91c079445847c2
kbtutorials <gauthamkarthik534@gmail.com>
2023-09-14 08:40:33 +0530

    HttpClient

--- src/main/java/com/HttpClientExplore/HttpClientExploreApplication.java
+++ src/main/java/com/HttpClientExplore/HttpClientExploreApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HttpClientExploreApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(HttpClientExploreApplication.class, args);
 	}
